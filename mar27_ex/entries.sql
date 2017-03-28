0389b7f113d8b3833dcc81934312fb7b03345851
CalderMarshall <cmarshall72@gmail.com>
2017-03-28 07:20:55 -0400

    sql exercises 1

---
+++ mar27_ex/entries.sql
@@ -0,0 +1,8 @@
+insert into restaurant values (default, 'Heirloom Market', 10.8, 4.7, 'bbq', 'brisket', '2016-10-15', TRUE);
+insert into restaurant values (default, 'Porch Light', 14, 5, 'cuban', 'porkchop', '2016-10-25', TRUE);
+insert into restaurant values (default, 'Bs BBQ', 10.8, 4, 'bbq', 'chicken', '2017-3-20', TRUE);
+insert into restaurant values (default, 'Hankook Taquiera', 8, 4, 'tacos', 'fries', '2017-1-15', TRUE);
+insert into restaurant values (default, 'Taquiera del Sol', 5, 5, 'tacos', 'fish tacos', '2016-4-06', TRUE);
+insert into restaurant values (default, 'Scalinis', 12, 3.5, 'italian', 'seafood trio', '2017-2-28', TRUE);
+insert into restaurant values (default, 'willys', 6, 3.5, 'tex-mex', 'burrito', '2017-2-20', TRUE);
+insert into restaurant values (default, 'M&T', 12, 5, 'classic', 'burger', '2016-11-20', TRUE);
---
+++ mar27_ex/restaurant_db.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE restaurant (
+id serial primary key,
+name varchar,
+distance numeric,
+stars numeric,
+category varchar,
+favoritedish varchar,
+lastate date,
+doestakeout boolean
+);
